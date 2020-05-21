73250cbac8b213963b753c2ca10541010c65f8b6
Saurav <spike.samantray@gmail.com>
2020-05-21 09:47:07 +0530

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+@app.route('/')
+def hello():
+    redis.incr('hits')
+    return render_template('index.html',hits=redis.get('hits').decode("utf-8"))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
